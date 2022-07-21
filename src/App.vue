17bd982e5930c6acf84615a07870b92212fe581b
Gqiang <zopiece@163.com>
2022-07-21 17:49:36 +0800

    fix:案例名字修改

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
       <router-link to="/materials_three">材质03</router-link> |
       <router-link to="/materials_four">材质04-平面着色</router-link> |
       <router-link to="/materials_five">材质05-显示正反面</router-link> |
-      <router-link to="/materials_six">材质06-显示正反面2(六个面)</router-link> |
+      <router-link to="/materials_six">材质06-六个面验证正反面显示</router-link> |
     </nav>
     <router-view/>
   </div>
